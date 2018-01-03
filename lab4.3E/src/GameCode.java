41931eb49a9adce088c848d11ce97c5decab35ad
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-01-03 12:14:33 -0500

    pushy

--- lab4.3E/src/GameCode.java
+++ lab4.3E/src/GameCode.java
@@ -1,8 +1,32 @@
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class GameCode {
 
 	public GameCode() {
 		// TODO Auto-generated constructor stub
 	}
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		primaryStage.setTitle("Simon Says");
+		FileInputStream pic1 = new FileInputStream("assets/png/r0.png");
+		Image image = new Image(pic1);
+		ImageView imageView = new ImageView(image);
+		Button button = new Button("Home",imageView);
+		 Scene scene = new Scene(button, 500, 500);
+	     primaryStage.setScene(scene);
+	     primaryStage.show();
+	}
+	public static void main(String[] args) 
+	{
+        Application.launch(args);
+    }
 
 }
