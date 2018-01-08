4f80cf7bf008ca38a06fcd30178e34481c078ce7
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-01-08 13:09:54 -0500

    pushy

--- lab4.3E/src/GameCode.java
+++ lab4.3E/src/GameCode.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class GameCode extends Application {
@@ -28,21 +29,36 @@ public class GameCode extends Application {
 	public void start(Stage primaryStage) throws Exception 
 	{
 		primaryStage.setTitle("Simon Says");
-		FlowPane pane = new FlowPane();
+		TilePane pane = new TilePane();
 		FileInputStream pic1 = new FileInputStream("assets/png/r0.png");
 		Image image = new Image(pic1);
 		ImageView imageView = new ImageView(image);
 		Button button = new Button("",imageView);
-		button.setMaxSize(400, 400);
-		button.setMinSize(400, 400);
+		button.setMaxSize(394, 400);
+		button.setMinSize(394, 400);
 		FileInputStream pic2 = new FileInputStream("assets/png/g0.png");
-		Image image2 = new Image(pic1);
+		Image image2 = new Image(pic2);
 		ImageView imageView2 = new ImageView(image2);
 		Button button2 = new Button("",imageView2);
-		button2.setMaxSize(400, 400);
-		button2.setMinSize(400, 400);
+		button2.setMaxSize(394, 400);
+		button2.setMinSize(394, 400);
+		FileInputStream pic3 = new FileInputStream("assets/png/b0.png");
+		Image image3 = new Image(pic3);
+		ImageView imageView3 = new ImageView(image3);
+		Button button3 = new Button("",imageView3);
+		button2.setMaxSize(394, 400);
+		button2.setMinSize(394, 400);
+		FileInputStream pic4 = new FileInputStream("assets/png/y0.png");
+		Image image4 = new Image(pic4);
+		ImageView imageView4 = new ImageView(image4);
+		Button button4 = new Button("",imageView4);
+		button2.setMaxSize(394, 400);
+		button2.setMinSize(394, 400);
 		pane.getChildren().add(button);
 		pane.getChildren().add(button2);
+		pane.getChildren().add(button3);
+		pane.getChildren().add(button4);
+		
 		 Scene scene = new Scene(pane, 1000, 1000);
 	     primaryStage.setScene(scene);
 	     primaryStage.show();
