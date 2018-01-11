584f2363a4fa1d235d11116ba65b84a9e3179d23
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-11 13:06:13 -0500

    dunzo garbunzo

--- lab4.3E/src/GameCode.java
+++ lab4.3E/src/GameCode.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
@@ -82,48 +83,89 @@ public class GameCode extends Application {
 		}.start();
 		primaryStage.setTitle("Simon Says");
 		TilePane pane = new TilePane();
-		FileInputStream pic1 = new FileInputStream("assets/png/r0.png");
-		Image image = new Image(pic1);
-		ImageView imageView = new ImageView(image);
-		Button button = new Button("",imageView);
-		button.setMaxSize(394, 400);
-		button.setMinSize(394, 400);
-		FileInputStream pic2 = new FileInputStream("assets/png/g0.png");
-		Image image2 = new Image(pic2);
-		ImageView imageView2 = new ImageView(image2);
-		Button button2 = new Button("",imageView2);
-		button2.setMaxSize(394, 400);
-		button2.setMinSize(394, 400);
-		FileInputStream pic3 = new FileInputStream("assets/png/b0.png");
-		Image image3 = new Image(pic3);
-		ImageView imageView3 = new ImageView(image3);
-		Button button3 = new Button("",imageView3);
-		button3.setMaxSize(394, 400);
-		button3.setMinSize(394, 400);
-		FileInputStream pic4 = new FileInputStream("assets/png/y0.png");
-		Image image4 = new Image(pic4);
-		ImageView imageView4 = new ImageView(image4);
-		Button button4 = new Button("",imageView4);
-		button4.setMaxSize(394, 400);
-		button4.setMinSize(394, 400);
-		FileInputStream picchange = new FileInputStream("assets/png/r1.png");
-		Image imagec = new Image(picchange);
-		ImageView imageViewc = new ImageView(imagec);
-		pane.getChildren().add(button);
-		pane.getChildren().add(button2);
-		pane.getChildren().add(button3);
-		pane.getChildren().add(button4);
 		
-		button.onMousePressedProperty(new EventHandler<ActionEvent>() 
+		Image red1 = new Image(new FileInputStream("assets/png/r1.png"));
+		Image red0 = new Image(new FileInputStream("assets/png/r0.png"));
+		ImageView redView0 = new ImageView(red0);
+		ImageView redView1 = new ImageView(red1);
+		Button red = new Button("",redView0);
+		red.setMaxSize(394, 400);
+		red.setMinSize(394, 400);
+		Image green1 = new Image(new FileInputStream("assets/png/g1.png"));
+		Image green0 = new Image(new FileInputStream("assets/png/g0.png"));
+		ImageView greenView0 = new ImageView(green0);
+		ImageView greenView1 = new ImageView(green1);
+		Button green = new Button("",greenView0);
+		green.setMaxSize(394, 400);
+		green.setMinSize(394, 400);
+		Image blue1 = new Image(new FileInputStream("assets/png/b1.png"));
+		Image blue0 = new Image(new FileInputStream("assets/png/b0.png"));
+		ImageView blueView0 = new ImageView(blue0);
+		ImageView blueView1 = new ImageView(blue1);
+		Button blue = new Button("",blueView0);
+		blue.setMaxSize(394, 400);
+		blue.setMinSize(394, 400);
+		Image yellow1 = new Image(new FileInputStream("assets/png/y1.png"));
+		Image yellow0 = new Image(new FileInputStream("assets/png/y0.png"));
+		ImageView yellowView0 = new ImageView(yellow0);
+		ImageView yellowView1 = new ImageView(yellow1);
+		Button yellow = new Button("",yellowView0);
+		yellow.setMaxSize(394, 400);
+		yellow.setMinSize(394, 400);
+		
+		pane.getChildren().add(red);
+		pane.getChildren().add(green);
+		pane.getChildren().add(blue);
+		pane.getChildren().add(yellow);
+		
+		red.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>()
 		{
-			  
-	            public void handle(ActionEvent event) 
-	            {
-	               button.setGraphic(imageViewc);
-	            }
-	    }
-		);
-		
+			public void handle(MouseEvent e) {
+				red.setGraphic(redView1);
+			}
+		});
+		red.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				red.setGraphic(redView0);
+			}
+		});
+		green.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				green.setGraphic(greenView1);
+			}
+		});
+		green.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				green.setGraphic(greenView0);
+			}
+		});
+		blue.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				blue.setGraphic(blueView1);
+			}
+		});
+		blue.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				blue.setGraphic(blueView0);
+			}
+		});
+		yellow.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				yellow.setGraphic(yellowView1);
+			}
+		});
+		yellow.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>()
+		{
+			public void handle(MouseEvent e) {
+				yellow.setGraphic(yellowView0);
+			}
+		});
 		 Scene scene = new Scene(pane, 788, 800);
 	     primaryStage.setScene(scene);
 	     primaryStage.show();
